b61ffa1437d8a931e216b8ef4effde79a1b0a32a
Chinthaka Batawala <senushkabatawala@gmail.com>
2021-07-23 17:52:47 +0530

    added required files.

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Nav from "../comp/nav.svelte";
+    </script>
+    <div class="p-8 max-w-6xl mx-auto">
+        <Nav/>
+        <slot>
+        </slot>
+    </div>
+    <style>
+    </style>
---
+++ src/routes/about.svelte
@@ -0,0 +1,2 @@
+<h1>About</h1>
+<p>This is a very bad pokemon gen, it just generates random pokenmon lmao.</p>
